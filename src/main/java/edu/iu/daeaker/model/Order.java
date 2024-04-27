3584ec6dda0b1ac4e81f28a7cbfd4baa7d37a8e9
daeaker <daeaker@iu.edu>
2024-04-27 14:28:59 -0400

    created Order

---
+++ src/main/java/edu/iu/daeaker/model/Order.java
@@ -0,0 +1,83 @@
+package edu.iu.daeaker.model;
+
+public class Order {
+
+    private int id;
+    private int customerId;
+    private String flowerName;
+    private int cost;
+    private String firstName;
+    private String lastName;
+    private String status;
+
+
+    public Order() {}
+
+
+    public Order(int id, int customerId, String flowerName, int cost, String firstName, String lastName, String status) {
+        this.id = id;
+        this.customerId = customerId;
+        this.flowerName = flowerName;
+        this.cost = cost;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.status = status;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFlowerName() {
+        return flowerName;
+    }
+
+    public void setFlowerName(String flowerName) {
+        this.flowerName = flowerName;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
